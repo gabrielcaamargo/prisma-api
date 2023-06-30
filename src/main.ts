f69d4bfb219d1a22d28070ea4b9e76c13fed2044
Gabriel Camargo <gcamargo.dev@gmail.com>
2023-06-30 16:10:12 -0300

    feat: create interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { NotFoundInterceptor } from './common/errors/interceptors/not-found.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +14,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new UnauthorizedInterceptor());
+  app.useGlobalInterceptors(new NotFoundInterceptor());
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
